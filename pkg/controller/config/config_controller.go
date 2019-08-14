e1a6417e4772c73c901cbfe172003ef155d97060
afoninsky <vkfont@yandex.ru>
2019-08-14 12:18:36 +0300

    remove stuff

--- pkg/controller/config/config_controller.go
+++ pkg/controller/config/config_controller.go
@@ -3,15 +3,11 @@ package config
 import (
 	"context"
 
-	kubelinkerv1alpha1 "github.com/afoninsky/kube-linker/pkg/apis/kubelinker/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -20,11 +16,6 @@ import (
 )
 
 var log = logf.Log.WithName("controller_config")
-
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
 
 // Add creates a new Config Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
@@ -39,29 +30,11 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
 	c, err := controller.New("config-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
-
-	// Watch for changes to primary resource Config
-	err = c.Watch(&source.Kind{Type: &kubelinkerv1alpha1.Config{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner Config
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &kubelinkerv1alpha1.Config{},
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return c.Watch(&source.Kind{Type: &extensionsv1beta1.Ingress{}}, &handler.EnqueueRequestForObject{})
 }
 
 // blank assignment to verify that ReconcileConfig implements reconcile.Reconciler
@@ -75,79 +48,25 @@ type ReconcileConfig struct {
 	scheme *runtime.Scheme
 }
 
-// Reconcile reads that state of the cluster for a Config object and makes changes based on the state read
-// and what is in the Config.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
-// Note:
+// Reconcile handles ingress updates
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcileConfig) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling Config")
+	reqLogger.Info("Ingress changed")
 
-	// Fetch the Config instance
-	instance := &kubelinkerv1alpha1.Config{}
+	// Fetch ingress
+	instance := &extensionsv1beta1.Ingress{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
+			// ingress is deleted
 			return reconcile.Result{}, nil
 		}
-		// Error reading the object - requeue the request.
+		// error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
+	// ingress is either created or updated
 
-	// Define a new Pod object
-	pod := newPodForCR(instance)
-
-	// Set Config instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-		err = r.client.Create(context.TODO(), pod)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-
-		// Pod created successfully - don't requeue
-		return reconcile.Result{}, nil
-	} else if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
 	return reconcile.Result{}, nil
-}
-
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *kubelinkerv1alpha1.Config) *corev1.Pod {
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
-		},
-	}
 }
--- readme.md
+++ readme.md
@@ -11,3 +11,13 @@ docker push $IMAGE
 https://github.com/operator-framework/operator-sdk-samples/tree/master/memcached-operator/test/e2e
 
 operator-sdk test local ./test
+
+
+### TODO
+[x] create ingress fetcher proof-of-concept
+[ ] handle valid ingresses: skip by labels, extract description, place to storage
+[ ] create web-server to display ingresses
+[ ] add some tests
+[ ] create deployment
+[ ] deploy to streamlayer
+[?] add virtualservices
